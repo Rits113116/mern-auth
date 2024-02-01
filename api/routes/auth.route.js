87cf5239aae6e88e9a92d353232d99700b99b466
Rits113116 <rr4618634@gmail.com>
2024-02-01 21:54:03 +0530

    create signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { signup } from '../contollers/auth.controller.js';
+import { signin } from '../contollers/auth.controller.js';
 
 const router=express();
 
 router.post("/signup",signup)
-
+router.post("/signin",signin)
 export default router;
