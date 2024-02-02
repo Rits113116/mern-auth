2ea1f0853e0f218028493b1c9807a16ac18c9363
Rits113116 <rr4618634@gmail.com>
2024-02-02 21:15:19 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Link,useNavigate } from "react-router-dom";
+import { signInStart,signInSuccess,signInFailure } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  const {loading,error}=useSelector((state)=>state.user);
   const navigate=useNavigate();
-
+  const dispatch=useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
     
@@ -14,8 +17,9 @@ export default function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false)
+      // setLoading(true);
+      // setError(false)
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -24,17 +28,22 @@ export default function Signin() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      setLoading(false);
+      // setLoading(false);
+  
       if(data.success===false)
       {
-        setError(true)
-        return
+        // setError(true)
+        // return
+        dispatch(signInFailure(data));
+        return;
       }
+      dispatch(signInSuccess(data))
       navigate('/')
       console.log(data);
     } catch (error) {
-      setLoading(false);
-      setError(true);
+      // setLoading(false);
+      // setError(true);
+      dispatch(signInFailure(error))
     }
   };
   return (
@@ -65,7 +74,7 @@ export default function Signin() {
           <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+      <p className="text-red-700 mt-5">{error? error.message ||"Something went wrong!":''}</p>
     </div>
   );
 }
